341b782286ebf60d915df56f4f13086b246f7841
bhaveshPatil <plbhavesh88@gmail.com>
2024-07-04 19:02:42 +0530

    removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -112,11 +112,11 @@ function App() {
             </form>
           </div>
         </div>
-        <footer className=" p-2 backdrop:blur-sm bg-white/20 absolute bottom-2 w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 rounded-2xl border border-gray-300">
+        {/* <footer className=" p-2 backdrop:blur-sm bg-white/20 absolute bottom-2 w-1/1 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 rounded-2xl border border-gray-300">
           <p className="text-white text-lg text-bold text-center">
             Copyright @{year} BhaveshP
           </p>
-        </footer>
+        </footer> */}
       </div>
     </>
   );
